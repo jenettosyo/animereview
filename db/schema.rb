29bc15ff1c463bc2aba004c8cf6afdf5c83d80a4
jenettosyo <jenettosyo@gmail.com>
2020-01-23 17:09:19 +0900

    user_image

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2020_01_22_135659) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image_name"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
