33a17e8922a47494e3d005c88d0aa47feed40715
jenettosyo <jenettosyo@gmail.com>
2020-01-23 16:10:20 +0900

    編集機能１

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,21 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(current_user.id)
+    @user = User.find_by(id: params[:id])
+
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(params[:id])
+    else
+      render action: edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name)
   end
 end
