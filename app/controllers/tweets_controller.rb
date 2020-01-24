452cf264d87d53244fcc605e475a9faf9b11cc59
jenettosyo <jenettosyo@gmail.com>
2020-01-24 14:10:07 +0900

    投稿機能

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,21 @@
 class TweetsController < ApplicationController
 
   def new
+    @tweet = Tweet.new
+    @user = User.find(current_user.id)
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save(tweet_params)
+      redirect_to (root_path)
+    else
+      render action: "new"
+    end
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:text, :picture).merge(user_id: current_user.id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_01_22_135659) do
 ActiveRecord::Schema.define(version: 2020_01_23_114716) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "text", null: false
-    t.string "image"
+    t.string "picture"
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
