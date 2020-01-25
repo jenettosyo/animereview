33eaf918bd8d82476aa74b82a403b6b0b82c22bb
jenettosyo <jenettosyo@gmail.com>
2020-01-25 22:26:02 +0900

    create_commentmodel

--- db/migrate/20200123114716_create_tweets.rb
+++ db/migrate/20200123114716_create_tweets.rb
@@ -2,7 +2,7 @@ class CreateTweets < ActiveRecord::Migration[5.2]
   def change
     create_table :tweets do |t|
 
-      t.string :text, null: false
+      t.text :text, null: false
       t.string :picture
       t.integer :user_id, null: false, foreign_key: true
       t.timestamps
