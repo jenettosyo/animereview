1ad8c2201b9765624cedba93074240b6434951e6
jenettosyo <jenettosyo@gmail.com>
2020-01-26 18:09:25 +0900

    validates設定

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,4 +3,6 @@ class Tweet < ApplicationRecord
   belongs_to :user
   has_many :comments
   mount_uploader :picture, PictureUploader
+  validates :text, presence: true
+  validates :picture, presence: true
 end
